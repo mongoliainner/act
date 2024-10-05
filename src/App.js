fc1424f0c189bfba67e218d39bfd1b93d10be29d
= <yuexrender@gmail.com>
2024-10-05 10:37:05 +0300

    tutorial: Creating And Reusing a Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
 
+function Image() {
+  return <h1>Duck</h1>;
+}
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Image></Image>
+    </div>
+  );
 }
 
 export default App;
