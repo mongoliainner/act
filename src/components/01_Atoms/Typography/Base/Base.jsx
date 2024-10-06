85b87da719f5f8df2c39282817ac4c98d1bd20c7
= <yuexrender@gmail.com>
2024-10-06 22:46:11 +0300

    update: typography styling

--- src/components/01_Atoms/Typography/Base/Base.jsx
+++ src/components/01_Atoms/Typography/Base/Base.jsx
@@ -1,9 +1,12 @@
 import React, { Children } from "react";
 import styles from "../../../0_Tokens/Boiler.css";
 
-function Base({ level, children }) {
-  const HeadingLevel = `h${level}`;
-  return <HeadingLevel>{children}</HeadingLevel>;
+function Base({ weight, className, children }) {
+  return (
+    <p className={className} style={{ fontWeight: weight }}>
+      {children}
+    </p>
+  );
 }
 
 export default Base;
--- src/components/01_Atoms/Typography/Headding/Headding.jsx
+++ src/components/01_Atoms/Typography/Headding/Headding.jsx
@@ -1,9 +1,14 @@
 import React, { Children } from "react";
 import styles from "../../../0_Tokens/Boiler.css";
+import HeaddingStyles from "./Heading.module.css";
 
-function Headding({ level, children }) {
+function Headding({ level, weight, className, children }) {
   const HeadingLevel = `h${level}`;
-  return <HeadingLevel>{children}</HeadingLevel>;
+  return (
+    <HeadingLevel className={className} style={{ fontWeight: weight }}>
+      {children}
+    </HeadingLevel>
+  );
 }
 
 export default Headding;
