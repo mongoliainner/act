3aa5a11d193a1a40ba5a17220382752ef4b78a5f
= <yuexrender@gmail.com>
2024-10-07 13:04:16 +0300

    update: css

--- src/components/01_Atoms/Typography/Headding/Headding.jsx
+++ src/components/01_Atoms/Typography/Headding/Headding.jsx
@@ -1,6 +1,5 @@
 import React, { Children } from "react";
 import styles from "../../../0_Tokens/Boiler.css";
-import HeaddingStyles from "./Heading.module.css";
 
 function Headding({ level, weight, className, children }) {
   const HeadingLevel = `h${level}`;
