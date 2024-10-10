cd85565b7d1d484576e4c4c8227ef13ce8df1921
= <yuexrender@gmail.com>
2024-10-10 17:45:11 +0300

    added: grid

---
+++ src/components/01_Atoms/Wrappers/Grid/Grid.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import styles from "./Grid.module.css";
+
+function Grid({
+  direction = "column",
+  wrap = "nowrap",
+  justifyContent = "flex-start",
+  alignItems = "flex-start",
+  paddingTop = "0",
+  paddingBottom = "0",
+  paddingLeft = "0",
+  paddingRight = "0",
+  gap = "10px",
+  width = "100%",
+  height = "100%",
+  className,
+  children,
+}) {
+  const combinedClassName = className
+    ? `${styles[className]} ${styles.text}`
+    : styles.flexWrapper;
+
+  return (
+    <div
+      className={combinedClassName}
+      style={{
+        flexDirection: direction,
+        flexWrap: wrap,
+        justifyContent: justifyContent,
+        alignItems: alignItems,
+        padding: `${paddingTop} ${paddingRight} ${paddingBottom} ${paddingLeft}`,
+        gap: gap,
+        width: width,
+        height: height,
+      }}
+    >
+      {children}
+    </div>
+  );
+}
+
+export default Grid;
